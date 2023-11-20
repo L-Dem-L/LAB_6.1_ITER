196a50727e2bd0a9c2db49c1d84ac34ebd31f0ba
Demian Rymar <ua.demyan@gmail.com>
2023-11-20 09:42:00 +0200

    Fixed

--- LAB_6.1_ITER/Main.cpp
+++ LAB_6.1_ITER/Main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <iomanip>
 
 void fillArray(int arr[]) {
-    for (int i = 0; i < 25; ++i) {
+    for (int i = 0; i < 25; i++) {
         arr[i] = rand() % (94 - 15 + 1) + 15;
     }
 }
 
 void printArray(const int arr[]) {
-    for (int i = 0; i < 25; ++i) {
-        std::cout << arr[i] << " ";
+    for (int i = 0; i < 25; i++) {
+        std::cout << std::setw(3) << arr[i] << " ";
     }
     std::cout << std::endl;
 }
@@ -19,8 +20,8 @@ void processArray(int arr[], int& count, int& sum) {
     count = 0;
     sum = 0;
 
-    for (int i = 0; i < 25; ++i) {
-        if (i % 2 != 0 && i % 13 != 0) {
+    for (int i = 0; i < 25; i++) {
+        if (!(arr[i] % 2 == 0 && i % 13 == 0)) {
             count++;
             sum += arr[i];
             arr[i] = 0;
@@ -39,10 +40,10 @@ int main() {
     printArray(myArray);
 
     processArray(myArray, count, sum);
-    std::cout << "Count of elements satisfying the criteria: " << count << std::endl;
-    std::cout << "Sum of elements satisfying the criteria: " << sum << std::endl;
     std::cout << "Modified Array: ";
     printArray(myArray);
+    std::cout << "Count of elements satisfying the criteria: " << count << std::endl;
+    std::cout << "Sum of elements satisfying the criteria: " << sum << std::endl;
 
     return 0;
 }
